adbbf6c6f03603174dbafe5e0903bb4c34d56a3a
Segis86 <segis1986@gmail.com>
2026-03-08 17:32:03 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/estaciones.json', (req,res)=>{
+
+const estaciones = [
+{
+nombre:"Villar del Arzobispo - Iglesia",
+lat:39.73,
+lon:-0.82,
+temp:12,
+link:"https://www.wunderground.com/dashboard/pws/IVILLA1255"
+},
+{
+nombre:"Benaguasil",
+lat:39.59,
+lon:-0.58,
+temp:14,
+link:"https://www.wunderground.com/dashboard/pws/IBENAG2"
+}
+];
+
+res.json(estaciones);
+
+});
+
+app.listen(PORT,()=>{
+console.log("Servidor funcionando");
+});
